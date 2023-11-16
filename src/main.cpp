9bf53ea176efc9a00de74b6d1ed60f3256e8ec77
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2023-11-17 00:37:37 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@
  */
 
 #include <iostream>
-#include "../include/task_2.h"
+#include "task_2.h"
 #include "../include/bubbleSort.h"
+#include "../include/task_3.h"
+#include "../include/task_4.h"
 using namespace std;
 
 int main() {
@@ -27,11 +29,42 @@ int main() {
         cout<<output[i]<<endl;
     }
     cout << "Task 2" << endl;
+    cout<<"Enter the number of coins: ";
+    cin>>n;
+    vector<int> numbers;
+    int number = 0;
+    for (int i=0;i<n;i++){
+        cin>>number;
+        numbers.push_back(number);
+    }
+    selectionSort(numbers);
+    for (int i=0;i<n;i++){
+        cout<<numbers[i]<<endl;
+    }
     // call for task 2
     cout << "Task 3" << endl;
     // call for task 3
+    tmp = "";
+    cout<<"Enter the number of soldiers: ";
+    cin>>n;
+    vector<string> soldiers;
+    for(int i=0;i<n;i++){
+        getline(cin>>ws, tmp);
+        soldiers.push_back(tmp);
+        tmp = "";
+    }
+    rearrangeSoldiers(soldiers);
     cout << "Task 4" << endl;
     // call for task 4
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    int size = n;
+    int arr[n];
+    for (int i=0;i<n;i++){
+        cin>>n;
+        arr[i] = n;
+    }
+    cout<<countElements(arr, size)<<endl;
     cout << "Task 5" << endl;
     // call for task 5
     return 0;
